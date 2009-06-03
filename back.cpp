aa871f95f8f522cee9693c4baecf07ed7a9d2617
cyberimp <cyberimp@ea9b064b-7d85-438e-bcad-835da519ca83>
2009-06-03 19:58:30 +0000

    bg sucks less

--- back.cpp
+++ back.cpp
@@ -11,17 +11,17 @@ void CBack::draw(){
   glPushMatrix();{
     glMatrixMode(GL_PROJECTION); 
     glLoadIdentity();
-    gluPerspective(45.0f, (GLfloat)GAME_FIELD_WIDTH/(GLfloat)GAME_FIELD_HEIGHT, 1.0f, 20.0f);
+    gluPerspective(45.0f, (GLfloat)GAME_FIELD_WIDTH/(GLfloat)GAME_FIELD_HEIGHT, 0.1f, 100.0f);
     glMatrixMode(GL_MODELVIEW); 
     glLoadIdentity();
-    //    glTranslatef(0.0f,.0f,0.0f);
+    glTranslatef(0.0f,-4.0f,-8.0f);
     glRotatef(-45.0f,1.0f,0.0f,0.0f);
     glBindTexture(GL_TEXTURE_RECTANGLE_ARB, ground_texture);
     glBegin(GL_QUADS);{
-      glTexCoord2i(0,flightY); glVertex3i(-1,-1,-1);
-      glTexCoord2i(GAME_FIELD_WIDTH,flightY); glVertex3i(1,-1,-1);
-      glTexCoord2i(GAME_FIELD_WIDTH,flightY+100); glVertex3i(1,1,-1);
-      glTexCoord2i(0, flightY+100);glVertex3i(-1,1,-1);
+      glTexCoord2i(0,flightY); glVertex3f(-10,0,-1);
+      glTexCoord2i(GAME_FIELD_WIDTH,flightY); glVertex3f(10,0,-1);
+      glTexCoord2i(GAME_FIELD_WIDTH,flightY+100); glVertex3f(10,21,-1);
+      glTexCoord2i(0, flightY+100);glVertex3f(-10,20,-1);
     }
     glEnd();
   }
