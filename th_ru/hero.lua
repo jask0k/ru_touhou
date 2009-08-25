f1ba5a4efcfaa15e6e0a4206f733a5217935aa7f
cyberimp <cyberimp@ea9b064b-7d85-438e-bcad-835da519ca83>
2009-08-25 12:52:44 +0000

    comments added

--- th_ru/hero.lua
+++ th_ru/hero.lua
@@ -1,12 +1,17 @@
-spritesheet_load("effects.png");
+-- hero.lua
+-- потом это будет несколько скриптов вида hero_xxx.lua
+-- в этом скрипте описываются всякие атаки и ништяки, специфичные для персонажа
 
-function hero_fire_begin()
---   log("otake!")
+spritesheet_load("effects.png"); -- загрузка спрайтового листа
+hero_name="ayayaya";
+
+function hero_fire_begin() -- эта функция вызывается при нажатии кнопки атаки
+--   log("otake!") -- запись в логе
+   begin_frame = engine_get_frame() -- сохраняем номер кадра, в котором начали стрелять
 end
 
-function hero_fire()
---   log("attack!");
-   if (engine_get_frame() % 3) == 0 then
+function hero_fire() -- эта функция вызывается каждый кадр, когда нажата кнопка атаки
+   if ((engine_get_frame()-begin_frame) % 3) == 0 then
       for i = 55,125,5 do
 	 sprite = sprite_create("bullets.png", LAYER_HERO_BULLET);
 	 sprite_set_position(sprite,hero_x(),hero_y(),i);
@@ -18,11 +23,11 @@ function hero_fire()
    end
 end
 
-function hero_fire_end()
+function hero_fire_end() -- эта функция вызывается когда отпускается кнопка атаки
 --   log("konec otake!");
 end
 
-function hero_focus_begin()
+function hero_focus_begin() -- эта функция вызывается когда нажимают кнопку фокуса
    hitbox = sprite_create("bullets.png", LAYER_HERO);
    sprite_set_follow (hitbox, hero_sprite());
    sprite_set_frame(hitbox,1);
@@ -37,13 +42,22 @@ function hero_focus_begin()
    sprite_set_scale_speed(focus,0.1);
 end
 
-function hero_focus()
---   sprite_set_position(focus,hero_x(),hero_y(),0);
+function hero_focus() -- эта функция вызывается каждый кадр во время фокуса
+
 end
 
-function hero_focus_end()
+function hero_focus_end() -- эта функция вызывается когда отпускают фокус
    sprite_destroy(hitbox);
    sprite_set_decay(focus,30);
    sprite_set_alpha_speed(focus,-.05);
    sprite_set_scale_speed(focus,-0.1);
+end
+
+function hero_graze() -- эта функция вызывается когда герой получает грейз
+   graze=graze+1;
+   log (graze);
+end
+
+function hero_die()
+   
 end
