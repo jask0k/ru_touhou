d7a229f094219276fb16c3b7d7a8ffbe26f03705
cyberimp <cyberimp@ea9b064b-7d85-438e-bcad-835da519ca83>
2009-09-05 13:13:21 +0000

    ai test

--- trunk/th_ru/level1.lua
+++ trunk/th_ru/level1.lua
@@ -7,6 +7,25 @@ graze = 0;
 power = 0;
 god_mode = 0;
 
+function lantern_AI(sprite, table)
+   sprite_set_position(sprite,-31,GAME_FIELD_HEIGHT-100,0);
+   sprite_set_speed(sprite,1,0,0);
+   sprite_start_animation(sprite,1,1);
+   wait(32);
+   while true do
+      local her_xpos = hero_x();
+      local her_ypos = hero_y();
+      local i
+      for i = 1, 5 do
+	 local lan_xpos,lan_ypos
+	 lan_xpos,lan_ypos = sprite_get_position(sprite)
+	 local bullet = enbullet_create_target(0,lan_xpos,lan_ypos,4,her_xpos,her_ypos,0);
+	 wait(10);
+      end
+      wait(10);
+   end
+end
+
 function sample_AI(bullet, table) -- исскуственный интеллект для управления спрайтами
 -- в первом параметре функции - хендл врага, управляемого интеллектом
 -- во втором - таблица, которую выдают каждому управляемому объекту
@@ -28,6 +47,7 @@ end
 
 -- Всякие загрузки-шмагрузки
 spritesheet_load("level1.png");
+spritesheet_load("lantern.png");
 log("Starting first level!");
 wait(1); -- Подождать кадр
 
@@ -46,7 +66,13 @@ sprite_set_alpha_speed(logo,-0.01); -- Разгущаем его
 wait(130); -- Ещё ждём
 
 
+for i = 1,5 do
+   lantern = sprite_create("lantern.png",LAYER_ENEMY);
+   bind_AI(CONTROL_SPRITE,lantern,lantern_AI);
+   wait(60);
+end
 
+--[[
 for k = 40, 340, 100 do
    sprite = enbullet_create(69,k,300,3,0); -- Пускаем пули
    bind_AI(CONTROL_BULLET,sprite,sample_AI);
@@ -57,3 +83,4 @@ end
 
 sprite = enbullet_create(69,100,300,3,0); -- Пускаем пули
 bind_AI(CONTROL_BULLET,sprite,sample_AI2);
+--]]
